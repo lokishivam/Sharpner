f2ef86264ceb421d6fa10e86548eba86eca571de
lokishivam <94531871+lokishivam@users.noreply.github.com>
2023-01-21 00:30:50 +0530

    call apply bind

--- solution.js
+++ solution.js
@@ -1,1 +1,25 @@
-console.log("hello world");
+function combine(x, y, z){
+
+    return this.age + x + y + z;
+  
+  }
+  
+  
+  
+  let stud = {
+  
+    age : 23
+  
+  }
+  
+  
+  
+  console.log(combine.call(stud, 3, 4, 5));
+  
+  console.log(combine.apply(stud, [4, 5, 6]));
+  
+  
+  
+  var bind = combine.bind(stud);
+  
+  console.log(bind(10, 20, 30));

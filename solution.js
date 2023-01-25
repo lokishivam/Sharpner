260b1e4a16aa986c68600bac070877b43fb2d6df
lokishivam <94531871+lokishivam@users.noreply.github.com>
2023-01-25 19:25:34 +0530

    3 Hours javascript program

--- solution.js
+++ solution.js
@@ -1,7 +1,39 @@
-function multiply(x, y){
-    return x*y;
+let form = document.getElementById("form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let pid = document.getElementById("productID").value;
+  let sp = document.getElementById("sp").value;
+  let pname = document.getElementById("productName").value;
+  let data = {
+    productid: pid,
+    sellingPrice: sp,
+    productName: pname,
+  };
+
+  console.log(pid);
+
+  let total = document.getElementById("total");
+  total.innerHTML = parseInt(total.innerHTML) + parseInt(sp);
+  localStorage.setItem(pid, JSON.stringify(data));
+
+  const node = document.createElement("li");
+  node.setAttribute("id", pid);
+  const textnode = document.createTextNode(`${pname} - ${sp}`);
+  node.appendChild(textnode);
+
+  const but = document.createElement("BUTTON");
+  but.setAttribute("onclick", `deleteItem(${pid})`);
+  const textnodebut = document.createTextNode("delete");
+  but.appendChild(textnodebut);
+
+  node.appendChild(but);
+  document.getElementById("myList").appendChild(node);
+});
+
+function deleteItem(pid) {
+  const element = document.getElementById(pid.id);
+  element.remove();
+  localStorage.removeItem(pid.id);
+  console.log(pid.id);
 }
-
-const multiplyByTwo = multiply.bind(this, 3);
-
-console.log(multiplyByTwo(2));
